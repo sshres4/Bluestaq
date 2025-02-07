7c38f8f7b91fb000e4ad0273b19c67c165740f22
sshres4 <shiva04.shrestha@gmail.com>
2025-02-06 23:04:17 -0600

    Update part3.sql

--- Database_Challenge_1/part3.sql
+++ Database_Challenge_1/part3.sql
@@ -1,14 +1,56 @@
-CREATE PROCEDURE InsertBook (
+CREATE PROCEDURE dbo.InsertBook
     @Title NVARCHAR(255),
-    @ISBN NVARCHAR(13),
-    @YearPublished INT,
-    @Genre NVARCHAR(100))
-
+    @ISBN NVARCHAR(255),
+    @Publisher NVARCHAR(255),
+    @PublicationYear INT,
+    @GenreName NVARCHAR(255),
+    @CopyCount INT,
+    @AuthorName NVARCHAR(255) 
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    -- Insert book into Books table
-    INSERT INTO Books (Title, ISBN, YearPublished, Genre)
-    VALUES (@Title, @ISBN, @YearPublished, @Genre);
-END;
+    DECLARE @GenreID INT;
+    DECLARE @BookID INT;
+    DECLARE @AuthorID INT;
+
+    -- Check if Genre exists, if not insert it
+    IF NOT EXISTS (SELECT 1 FROM Genre WHERE Name = @GenreName)
+    BEGIN
+        INSERT INTO Genre (Name)
+        VALUES (@GenreName);
+    END
+
+    -- Get GenreID
+    SELECT @GenreID = GenreID FROM Genre WHERE Name = @GenreName;
+
+    -- Insert into Book table
+    INSERT INTO Book (Title, ISBN, Publisher, PublicationYear, GenreID, CopyCount)
+    VALUES (@Title, @ISBN, @Publisher, @PublicationYear, @GenreID, @CopyCount);
+
+    -- Get the newly inserted BookID
+    SELECT @BookID = SCOPE_IDENTITY();
+
+    -- Insert into Copies table
+    DECLARE @i INT = 1;
+    WHILE @i <= @CopyCount
+    BEGIN
+        INSERT INTO Copies (BookID, Status)
+        VALUES (@BookID, 'Available');
+        SET @i = @i + 1;
+    END
+
+    -- Check if Author exists, if not insert it
+    IF NOT EXISTS (SELECT 1 FROM Author WHERE Name = @AuthorName)
+    BEGIN
+        INSERT INTO Author (Name)
+        VALUES (@AuthorName);
+    END
+
+    -- Get AuthorID
+    SELECT @AuthorID = AuthorID FROM Author WHERE Name = @AuthorName;
+
+    -- Insert into BookAuthor table
+    INSERT INTO BookAuthor (BookID, AuthorID)
+    VALUES (@BookID, @AuthorID);
+END
