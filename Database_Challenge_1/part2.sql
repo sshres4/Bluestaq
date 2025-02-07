946123925c98630e11c694d95d7b7624053ae64f
sshres4 <shiva04.shrestha@gmail.com>
2025-02-06 23:03:21 -0600

    Update part2.sql

--- Database_Challenge_1/part2.sql
+++ Database_Challenge_1/part2.sql
@@ -1,39 +1,108 @@
-Create Table Books(
-	BookID INT IDENTITY(1,1) PRIMARY KEY,
-	Title NVarchar(255),
-	ISBN NVarchar(13),
-	YearPublished INT,
-	Genre nvarchar(100)
-);
+/*
+For the library management system, I have created following entities: Book, Author, Genre, BookAuthor, Copies, Borrower, Borrow, Hold, Waitlist
+Reason for creating the extra tables were to handle standard cases where members were borrowing a book, putting a hold, creating a waitlist, tracking different copies of the same book etc
+Below are the reuseable scripts to generate the table schema for the entities
 
-Create Table Authors(
-	AuthorID INT IDENTITY(1,1) PRIMARY KEY,
-	Name NVarchar(255),
-	Bio NVarchar(1000)
-);
+*/
 
-Create Table Borrowers(
-	BorrowerID INT IDENTITY(1,1) PRIMARY KEY,
-	Name NVarchar(255),
-	Email NVarchar(255),
-	PhoneNumber NVarchar(20),
-	Address NVarchar(255)
-);
 
-Create Table Book_Authors(
-	BookID INT,
-	AuthorID INT,
-	PRIMARY KEY (BookID, AuthorID),
-	FOREIGN KEY (BookID) REFERENCES Books(BookID) ON DELETE CASCADE,
-	FOREIGN KEY (AuthorID) REFERENCES Authors(AuthorID) ON DELETE CASCADE
-);
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Genre]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE [dbo].[Genre] (
+        GenreID INT IDENTITY(1,1) PRIMARY KEY,
+        Name NVARCHAR(255) NOT NULL
+    );
+END
 
-Create Table Borrowed_Books(
-	BorrowerID INT,
-	BookID INT,
-	BorrowDate Date,
-	ReturnDate Date,
-	PRIMARY KEY (BookID, BorrowerID, BorrowDate),
-	FOREIGN KEY (BookID) REFERENCES Books(BookID) ON DELETE CASCADE,
-	FOREIGN KEY (BorrowerID) REFERENCES Borrowers(BorrowerID) ON DELETE CASCADE
-);
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Book]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE [dbo].[Book] (
+        BookID INT IDENTITY(1,1) PRIMARY KEY,
+        Title NVARCHAR(255) NOT NULL,
+        ISBN NVARCHAR(13) NOT NULL,
+        Publisher NVARCHAR(255) NOT NULL,
+        PublicationYear INT NOT NULL,
+        GenreID INT NOT NULL,
+        FOREIGN KEY (GenreID) REFERENCES [dbo].[Genre](GenreID),
+		CopyCount INT NOT NULL
+    );
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Author]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE [dbo].[Author] (
+        AuthorID INT IDENTITY(1,1) PRIMARY KEY,
+        Name NVARCHAR(255),
+        Bio NVARCHAR(MAX)
+    );
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Borrower]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE [dbo].[Borrower] (
+        BorrowerID INT IDENTITY(1,1) PRIMARY KEY,
+        Name NVARCHAR(255),
+        Address NVARCHAR(255),
+        PhoneNumber NVARCHAR(15)
+    );
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Copies]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE [dbo].[Copies] (
+        CopyID INT IDENTITY(1,1) PRIMARY KEY,
+        BookID INT,
+        Status NVARCHAR(50),
+        FOREIGN KEY (BookID) REFERENCES [dbo].[Book](BookID)
+    );
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[BookAuthor]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE [dbo].[BookAuthor] (
+        BookID INT,
+        AuthorID INT,
+        PRIMARY KEY (BookID, AuthorID),
+        FOREIGN KEY (BookID) REFERENCES [dbo].[Book](BookID),
+        FOREIGN KEY (AuthorID) REFERENCES [dbo].[Author](AuthorID)
+    );
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Borrow]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE [dbo].[Borrow] (
+        BorrowID INT IDENTITY(1,1) PRIMARY KEY,
+        BorrowerID INT,
+        CopyID INT,
+        BorrowDate DATE,
+        ReturnDate DATE,
+		DueDate DATE,
+        FOREIGN KEY (BorrowerID) REFERENCES [dbo].[Borrower](BorrowerID),
+        FOREIGN KEY (CopyID) REFERENCES [dbo].[Copies](CopyID)
+    );
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Hold]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE [dbo].[Hold] (
+        HoldID INT IDENTITY(1,1) PRIMARY KEY,
+        BorrowerID INT,
+        CopyID INT,
+        HoldDate DATE,
+        FOREIGN KEY (BorrowerID) REFERENCES [dbo].[Borrower](BorrowerID),
+        FOREIGN KEY (CopyID) REFERENCES [dbo].[Copies](CopyID)
+    );
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Waitlist]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE [dbo].[Waitlist] (
+        WaitlistID INT IDENTITY(1,1) PRIMARY KEY,
+        BorrowerID INT,
+        BookID INT,
+        RequestDate DATE,
+        FOREIGN KEY (BorrowerID) REFERENCES [dbo].[Borrower](BorrowerID),
+        FOREIGN KEY (BookID) REFERENCES [dbo].[Book](BookID)
+    );
+END
